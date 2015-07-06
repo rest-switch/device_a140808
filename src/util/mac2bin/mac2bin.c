1c417ade652ffddca52dd8f82628f124717ba70e
John Clark <inindev@gmail.com>
2015-07-06 01:55:45 -0400

    mac2bin initial commit

---
+++ src/util/mac2bin/mac2bin.c
@@ -0,0 +1,48 @@
+//
+// Copyright 2015 The REST Switch Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, Licensor provides the Work (and each Contributor provides its 
+// Contributions) on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied, including, 
+// without limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A PARTICULAR 
+// PURPOSE. You are solely responsible for determining the appropriateness of using or redistributing the Work and assume any 
+// risks associated with Your exercise of permissions under this License.
+//
+// Author: John Clark (johnc@restswitch.com)
+//
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void printbin(const int val)
+{
+    const char h = ((val >> 8) & 0x00ff);
+    const char l = (val & 0x00ff);
+    printf("%c%c", l, h);
+}
+
+int main(int argc, char** argv)
+{
+    if(2 != argc) return(1);
+
+    const char* str = argv[1];
+    const int len = strlen(str);
+    if(12 != len) return(2);
+
+    char* endptr = 0;
+    const long long int val = strtol(str, &endptr, 16);
+    if(str == endptr) {
+        return(3);
+    }
+
+    printbin(val >> 32);
+    printbin(val >> 16);
+    printbin(val);
+}
